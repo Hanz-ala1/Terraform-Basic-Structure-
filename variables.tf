d4f36d2e0e95b6b0db54db4df4135468d572d671
Mohammed <hanzala.mohammed@capgemini.com>
2023-02-18 23:50:10 +0000

    Outputs with count

--- variables.tf
+++ variables.tf
@@ -13,10 +13,18 @@ variable "instance_type" {
   type        = string
   default     = "t2.micro"
 }
-
-#aws EC2 Instance Key Pair
-#variable "instance_keypair" {
-  #description = "AWS EC2 Key Pair that need to be associated with EC2 Instance"
-  #type = string
-  #default = "terraform-key"
-#}
+#AWS EC2 Instance Type -List
+variable "instance_type_list" {
+  description = "ec2 list"
+  type        = list(string)
+  default     = ["t2.micro", "t1.micro"]
+}
+# AWS EC2 Instance Type - Map
+variable "instance_type_map" {
+  description = "EC2 Instance Type"
+  type        = map(string)
+  default = {
+    "dev" = "t2.micro"
+    "qa"  = "t1.micro"
+  }
+}
